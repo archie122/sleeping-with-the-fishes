03e580ed7fc2f4a1e536776d3abe15e792390448
Archie <110494298+archie122@users.noreply.github.com>
2025-01-11 18:45:02 -0700

    Got things work :D

--- sleeping-with-the-fishes/app/(tabs)/two.tsx
+++ sleeping-with-the-fishes/app/(tabs)/two.tsx
@@ -3,12 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>This is a test. If this changes, that means everything is working beautifully. :D</Text>
+      <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
